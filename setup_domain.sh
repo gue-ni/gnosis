03072fb17634daabc4536995a843ca18629fe3f0
Jakob <jakob.g.maier@gmail.com>
2021-05-21 21:40:24 +0200

    added analytics script

--- setup_domain.sh
+++ setup_domain.sh
@@ -6,10 +6,22 @@ echo $domain
 sudo -i
 
 mkdir -p /var/log/nginx/$domain
+
 mkdir -p /var/www/$domain
+
 echo $domain > /var/www/$domain/index.html
 
 touch /etc/nginx/sites-available/$domain.conf
+
+wget https://raw.githubusercontent.com/gue-ni/raspberrypi-server/main/example.com.conf -O /etc/nginx/sites-available/$domain.conf
+
+sed -i "s/example.com/$domain/g" /etc/nginx/sites-available/$domain.conf
+
 ln -s /etc/nginx/sites-available/$domain.conf /etc/nginx/sites-enabled/
-nginx -t
-nginx -s reload
+
+nginx -t && nginx -s reload
+
+certbot --nginx -d $domain -d www.$domain
+
+nginx -t && nginx -s reload
+
