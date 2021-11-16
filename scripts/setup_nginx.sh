c8deb090d8d89acecfbac54ede977d409dbc1a9b
Jakob <45669207+gue-ni@users.noreply.github.com>
2021-11-16 12:58:47 +0100

    Update and rename configure_nginx.sh to setup_nginx.sh

---
+++ scripts/setup_nginx.sh
@@ -0,0 +1,32 @@
+
+#!/bin/bash
+
+domain=${1}
+
+if [ -z "${domain}" ] ; then
+  echo "ERROR: domain not set!"
+  echo "Aborting."
+  exit 1
+fi
+
+github=https://raw.githubusercontent.com/gue-ni/server/main
+
+mkdir -p /var/log/nginx/$domain/ /var/www/$domain/
+wget $github/conf/index.html -O /var/www/$domain/index.html
+sed -i "s/example.com/$domain/g" /var/www/$domain/index.html
+wget $github/conf/example.com.conf -O /etc/nginx/sites-available/$domain.conf
+sed -i "s/example.com/$domain/g" /etc/nginx/sites-available/$domain.conf
+ln -s /etc/nginx/sites-available/$domain.conf /etc/nginx/sites-enabled/
+
+nginx -t && nginx -s reload
+
+read -p "Add TLS? (y/n)?" CONT
+if [ "$CONT" = "n" ]; then
+  exit 1
+fi
+
+certbot --nginx -d $domain -d www.$domain -d api.$domain
+
+nginx -t && nginx -s reload
+
+
