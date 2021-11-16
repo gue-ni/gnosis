9a8cf78cd7056ffa1e5319991dd09cd0b0188fe0
Jakob <45669207+gue-ni@users.noreply.github.com>
2021-11-16 13:19:04 +0100

    Update setup_nginx_server_block_for_domain.sh

--- scripts/setup_nginx_server_block_for_domain.sh
+++ scripts/setup_nginx_server_block_for_domain.sh
@@ -8,15 +8,21 @@ fi
 
 github=https://raw.githubusercontent.com/gue-ni/DevOps/main
 
+# create directories
 mkdir -p /var/log/nginx/$domain/ /var/www/$domain/
+
+# create template website
 wget $github/conf/index.html -O /var/www/$domain/index.html
 sed -i "s/example.com/$domain/g" /var/www/$domain/index.html
+
+# create nginx conf files
 wget $github/conf/example.com.conf -O /etc/nginx/sites-available/$domain.conf
 sed -i "s/example.com/$domain/g" /etc/nginx/sites-available/$domain.conf
 ln -s /etc/nginx/sites-available/$domain.conf /etc/nginx/sites-enabled/
 
 nginx -t && nginx -s reload
 
+# optionally add https
 read -p "Add TLS? (y/n)?" CONT
 if [ "$CONT" = "n" ]; then
   exit 1
