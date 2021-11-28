241516ac15cf008ce428919647916f53e8e17111
Jakob <45669207+gue-ni@users.noreply.github.com>
2021-11-28 12:54:00 +0100

    Update rsync_backup.sh

--- scripts/rsync_backup.sh
+++ scripts/rsync_backup.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+INCLUDE=include.txt
+EXCLUDE=exclude.txt
+
+if [ -z $INCLUDE ]; then
+    echo "ERROR: please give include file"
+    exit 1 
+fi
+
+if [ -z $EXCLUDE ]; then
+    echo "ERROR: please give exclude file"
+    exit 1 
+fi
+
 PI1=pi1
 TARGET=/media/pi/KINGSTON1/VivoBook/
 
 HOST=$PI1
 SERVER=$HOST:$TARGET
 
-OPTIONS="-rtDviR --delete --modify-window=2 --exclude-from=exclude.txt --max-size=100MB"
+OPTIONS="-rtDviR --delete --modify-window=2 --exclude-from=$EXCLUDE --max-size=100MB"
 
 LOGDIR=~/logs
 LOGFILE=$LOGDIR/backup_$(date "+%F-%H%M").log
-
 mkdir -p $LOGDIR
 
 while read SRC;
@@ -18,4 +30,4 @@ do
     echo "###############################################################" | tee -a $LOGFILE
     echo $SRC: | tee -a $LOGFILE
     rsync $OPTIONS $SRC $SERVER | tee -a $LOGFILE
-done < include.txt
+done < $INCLUDE
