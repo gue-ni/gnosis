add60c3b90cfe2dceda97c66e7fbf1973d9d63e6
Jakob <45669207+gue-ni@users.noreply.github.com>
2021-12-26 17:02:20 +0100

    Update setup_nginx_server_block_for_domain.sh

--- scripts/setup_nginx_server_block_for_domain.sh
+++ scripts/setup_nginx_server_block_for_domain.sh
@@ -7,16 +7,17 @@ if [ "$domain" = "" ]; then
 fi
 
 github=https://raw.githubusercontent.com/gue-ni/DevOps/main
+https://raw.githubusercontent.com/gue-ni/DevOps/main/configs/example.com.conf
 
 # create directories
 mkdir -p /var/log/nginx/$domain/ /var/www/$domain/
 
 # create template website
-wget $github/conf/index.html -O /var/www/$domain/index.html
+wget $github/configs/index.html -O /var/www/$domain/index.html
 sed -i "s/example.com/$domain/g" /var/www/$domain/index.html
 
 # create nginx conf files
-wget $github/conf/example.com.conf -O /etc/nginx/sites-available/$domain.conf
+wget $github/configs/example.com.conf -O /etc/nginx/sites-available/$domain.conf
 sed -i "s/example.com/$domain/g" /etc/nginx/sites-available/$domain.conf
 ln -s /etc/nginx/sites-available/$domain.conf /etc/nginx/sites-enabled/
 
