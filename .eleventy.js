9557b9795396d6f42855ed074250541701adb8e8
Jakob <45669207+gue-ni@users.noreply.github.com>
2023-10-23 11:04:20 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,16 @@
+const markdownItAnchor = require("markdown-it-anchor");
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy("scripts");
   eleventyConfig.addPassthroughCopy("configs");
   eleventyConfig.addPassthroughCopy("books");
   eleventyConfig.addPassthroughCopy("static");
   eleventyConfig.addPassthroughCopy("{images,configs,doc}/**/*.{png,jpg,jpeg,ico,gif,css,js,mp4}");
+
+  const markdown = markdownIt({
+		html: true,
+	});
+
+	markdown.use(markdownItAnchor);
+	eleventyConfig.setLibrary("md", markdown)
 };
