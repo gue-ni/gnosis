2036962a74f0d5f3696a230a5d13397c6ec5afec
jama274470 <Jakob.Maier@elektrobit.com>
2024-01-09 09:24:07 +0100

    update

--- doc/jira.md
+++ doc/jira.md
@@ -2,6 +2,10 @@
 
 ## Filters
 
-```
-project = ASCSOAD AND assignee was currentUser() 
-```
+### all tickets assigned to me
+
+`assignee in (currentUser())`
+
+### all tickets in project that were ever assigned to me
+
+`project = ASCSOAD AND assignee was currentUser()`
---
+++ scripts/AUTOSAR.h
@@ -0,0 +1,22 @@
+#ifndef AUTOSAR_H
+#define AUTOSAR_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+
+typedef uint8_t                                     uint8;
+typedef uint16_t                                    uint16;
+typedef uint32_t                                    uint32;
+typedef uint_least8_t                               uint8_least;
+typedef uint_least16_t                              uint16_least;
+typedef uint_least32_t                              uint32_least;
+
+#define AUTOMATIC                                   /* memclass */
+#define SOAD_APPL_DATA                              /* ptrclass */
+#define P2VAR(ptrtype, memclass, ptrclass)          ptrtype * memclass ptrclass
+#define P2CONST(ptrtype, memclass, ptrclass)        ptrtype const * memclass ptrclass
+#define CONSTP2VAR(ptrtype, memclass, ptrclass)     ptrtype * const memclass ptrclass
+#define CONSTP2CONST(ptrtype, memclass, ptrclass)   ptrtype const * const memclass ptrclass
+
+#endif /* AUTOSAR_H */
